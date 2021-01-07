05ccbadfa11f194bb2a2cfba47a4a1c16fca2489
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 20:32:15 +0200

    Add files via upload

---
+++ Task03A_Combinations/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task03A_Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int number = int.Parse(Console.ReadLine());
+            for (int x1 = 0; x1 <= number; x1++)
+            {
+                for (int x2 = 0; x2 <= number; x2++)
+                {
+                    for (int x3 = 0; x3 <= number; x3++)
+                    {
+                        if (x1 + x2 + x3 == number) count += 1;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
