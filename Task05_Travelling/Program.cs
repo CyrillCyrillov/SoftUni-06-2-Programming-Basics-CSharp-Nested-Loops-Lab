10a0acfd282e9e7ece265bf573c2acf6f800b130
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 20:34:10 +0200

    Add files via upload

---
+++ Task05_Travelling/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task05_Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nextSum = 0;
+            while (true)
+            {
+                string destination = Console.ReadLine();
+                if (destination == "End") break;
+                double sum = double.Parse(Console.ReadLine());
+                double savedMoney = 0;
+                while (savedMoney < sum)
+                {
+                    nextSum = double.Parse(Console.ReadLine());
+                    savedMoney += nextSum;
+                }
+                Console.WriteLine($"Going to {destination}!");
+            }
+        }
+    }
+}
